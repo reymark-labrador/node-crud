08c6d89ad3abff60b33a9022fb101d99725d85e6
mac69 <labrador.reymark@gmail.com>
2021-07-24 17:14:07 +0400

    Add Dockerfile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,15 +2,23 @@ const router = require('express').Router();
 const db = require("../models");
 const User = db.user;
 
-router.get('/user/:id', async(req, res) => {
-    const user = await User.findByPk(req.params.id);
-    
-    res.send(user);
+router.get('/user/:id', async(req, res, next) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        res.send(user);
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get('/users', async(req, res) => {
-    const users = await User.findAll();
-    res.send(users);
+router.get('/users', async(req, res, next) => {
+    try {
+        console.log(process.env.NODE_ENV, "env");
+        const users = await User.findAll();
+        res.send(users);
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
